be8314fc25531bfd7976b764ff352078fcf77df7
Kyle Allan <kyle@stitchdata.com>
2018-07-20 12:26:40 -0400

    dont format special characters

--- tap_s3_csv/csv_handler.py
+++ tap_s3_csv/csv_handler.py
@@ -13,7 +13,7 @@ def generator_wrapper(reader):
             formatted_key = key
 
             # remove non-word, non-whitespace characters
-            formatted_key = re.sub(r"[^\w\s]", '', formatted_key)
+            #formatted_key = re.sub(r"[^\w\s]", '', formatted_key)
 
             to_return[formatted_key] = value
 
