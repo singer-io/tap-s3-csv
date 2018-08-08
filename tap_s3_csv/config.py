fc2ef1184d0062bc502b070fe896d09025e3e780
Kyle Allan <kyle@stitchdata.com>
2018-08-08 14:29:14 -0400

    allow delimiter to pass through to singer-encodings

--- tap_s3_csv/config.py
+++ tap_s3_csv/config.py
@@ -5,5 +5,6 @@ CONFIG_CONTRACT = Schema([{
     Required('search_pattern'): str,
     Required('key_properties'): [str],
     Optional('search_prefix'): str,
-    Optional('date_overrides'): [str]
+    Optional('date_overrides'): [str],
+    Optional('delimiter'): str
 }])
