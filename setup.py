1514b2b81a3d328a18e5336a5b67c28d96a8bc1b
Tim Visher <tim@stitchdata.com>
2019-03-27 19:35:23 +0000

    1.0.4 → 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-s3-csv',
-      version='1.0.4',
+      version='1.1.0',
       description='Singer.io tap for extracting CSV files from S3',
       author='Stitch',
       url='https://singer.io',
