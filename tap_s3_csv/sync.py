6aa529eef1e9b9b3342f498b6ad77ea772b803f7
dbshah1212 <root@ubuntu16.cdsys.local>
2021-06-03 11:52:27 +0530

    TDL-12589: Changed sdc_extra log from debug to warn

--- tap_s3_csv/sync.py
+++ tap_s3_csv/sync.py
@@ -214,7 +214,7 @@ def sync_jsonl_file(config, iterator, s3_path, table_spec, stream):
         value = [ {field:rec[field]} for field in set(rec) - set(to_write) ]
 
         if value:
-            LOGGER.debug(
+            LOGGER.warning(
                 "The schema does not have \"%s\" so its entry got removed in transformation and is stored in \"_sdc_extra\" field.", value)
             extra_data = {
                 s3.SDC_EXTRA_COLUMN: value
