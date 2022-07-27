1c0e603c11b9f3db530a2dabf13eead4027283f9
harshpatel4crest <harsh.patel4@crestdatasys.com>
2022-07-27 12:05:28 +0530

    updated max field width test case

--- tests/test_maximize_csv_field_width.py
+++ tests/test_maximize_csv_field_width.py
@@ -32,3 +32,20 @@ class S3MaxFieldWidthCSV(S3CSVBaseTest):
 
         found_catalog_names = set(map(lambda c: c['tap_stream_id'], found_catalogs))
         self.assertEqual(self.expected_check_streams(), found_catalog_names, msg="Expected check streams are not subset of discovered catalog")
+
+        # Select our catalogs
+        our_catalogs = [c for c in found_catalogs if c.get('tap_stream_id') in self.expected_check_streams()]
+        self.perform_and_verify_table_and_field_selection(self.conn_id, our_catalogs, True)
+
+        # Run a sync job using orchestrator
+        self.run_and_verify_sync(self.conn_id)
+
+        expected_records = 1
+        record_count = runner.get_upserts_from_target_output()
+        # Verify record counts
+        self.assertEqual(expected_records, len(record_count))
+
+        records = runner.get_records_from_target_output()
+        actual_records = [record.get('data') for record in records.get('csv_with_max_field_width').get('messages')]
+        # Verify the records
+        self.assertEqual(actual_records, [{'id': 1, 'name': '{}'.format('a'*131074), '_sdc_source_bucket': 'com-stitchdata-prod-circleci-assets', '_sdc_source_file': 'tap_tester/max_size.csv', '_sdc_source_lineno': 2}])
