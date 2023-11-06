f038895fd0481b406e9bc0df0af24433b2bcdd17
jayma91 <86244437+jayma91@users.noreply.github.com>
2023-11-06 11:47:14 -0500

    WP-17438 Error handling clean up (#52)
    * WP-17438 error message, traceback update
    
    * WP-17438 error logging fix

--- tap_s3_csv/__init__.py
+++ tap_s3_csv/__init__.py
@@ -20,6 +20,10 @@ REQUIRED_CONFIG_KEYS_EXTERNAL_SOURCE = [
 
 IMPORT_PERF_METRICS_LOG_PREFIX = "IMPORT_PERF_METRICS:"
 
+# for symon error logging
+ERROR_START_MARKER = '[tap_error_start]'
+ERROR_END_MARKER = '[tap_error_end]'
+
 
 def do_discover(config):
     LOGGER.info("Starting discover")
@@ -128,35 +132,42 @@ def main():
         elif args.properties:
             do_sync(config, args.properties, args.state)
     except SymonException as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
         error_info = {
-            'message': str(e),
+            'message': traceback.format_exception_only(exc_type, exc_value)[-1],
             'code': e.code,
-            'traceback': traceback.format_exc()
+            'traceback': "".join(traceback.format_tb(exc_traceback))
         }
 
         if e.details is not None:
             error_info['details'] = e.details
         raise
     except BaseException as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
         error_info = {
-            'message': str(e),
-            'traceback': traceback.format_exc()
+            'message': traceback.format_exception_only(exc_type, exc_value)[-1],
+            'traceback': "".join(traceback.format_tb(exc_traceback))
         }
         raise
     finally:
         if error_info is not None:
-            error_file_path = args.config.get('error_file_path', None)
-            if error_file_path is not None:
-                try:
-                    with open(error_file_path, 'w', encoding='utf-8') as fp:
-                        json.dump(error_info, fp)
-                except:
-                    pass
-            # log error info as well in case file is corrupted
-            error_info_json = json.dumps(error_info)
-            error_start_marker = args.config.get('error_start_marker', '[tap_error_start]')
-            error_end_marker = args.config.get('error_end_marker', '[tap_error_end]')
-            LOGGER.info(f'{error_start_marker}{error_info_json}{error_end_marker}')
+            try:
+                error_file_path = args.config.get('error_file_path', None)
+                if error_file_path is not None:
+                    try:
+                        with open(error_file_path, 'w', encoding='utf-8') as fp:
+                            json.dump(error_info, fp)
+                    except:
+                        pass
+                # log error info as well in case file is corrupted
+                error_info_json = json.dumps(error_info)
+                error_start_marker = args.config.get('error_start_marker', ERROR_START_MARKER)
+                error_end_marker = args.config.get('error_end_marker', ERROR_END_MARKER)
+                LOGGER.info(f'{error_start_marker}{error_info_json}{error_end_marker}')
+            except:
+                # error occurred before args was parsed correctly, log the error
+                error_info_json = json.dumps(error_info)
+                LOGGER.info(f'{ERROR_START_MARKER}{error_info_json}{ERROR_END_MARKER}')
 
 
 if __name__ == '__main__':
