af3d0956473f25ef054ce766848ee0e99a4410a7
“rdeshmukh15” <rdeshmukh@talend.com>
2024-03-21 12:55:28 +0000

    hardcoded region name

--- tap_s3_csv/s3.py
+++ tap_s3_csv/s3.py
@@ -489,7 +489,7 @@ def get_request_timeout(config):
 def list_files_in_bucket(config, search_prefix=None):
     # Set connect and read timeout for resource
     timeout = get_request_timeout(config)
-    client_config = Config(connect_timeout=timeout,  read_timeout=timeout)
+    client_config = Config(connect_timeout=timeout,  read_timeout=timeout, region_name='me-south-1')
     s3_client = boto3.client('s3', config=client_config)
 
     s3_object_count = 0
