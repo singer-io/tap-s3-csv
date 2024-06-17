7d03dbff75ba73ad2fa65a11eb7d49d7787d8a53
“rdeshmukh15” <rdeshmukh@talend.com>
2024-06-17 04:20:38 +0000

    changes to handle s3 files modified date race condition

--- tap_s3_csv/sync.py
+++ tap_s3_csv/sync.py
@@ -20,7 +20,7 @@ from tap_s3_csv import (
 
 LOGGER = singer.get_logger()
 
-def sync_stream(config, state, table_spec, stream):
+def sync_stream(config, state, table_spec, stream, sync_start_time):
     table_name = table_spec['table_name']
     modified_since = singer_utils.strptime_with_tz(singer.get_bookmark(state, table_name, 'modified_since') or
                                             config['start_date'])
@@ -41,8 +41,8 @@ def sync_stream(config, state, table_spec, stream):
         records_streamed += sync_table_file(
             config, s3_file['key'], table_spec, stream)
 
-        state = singer.write_bookmark(state, table_name, 'modified_since', s3_file['last_modified'].isoformat())
-        singer.write_state(state)
+    state = singer.write_bookmark(state, table_name, 'modified_since', sync_start_time)
+    singer.write_state(state)
 
     if s3.skipped_files_count:
         LOGGER.warn("%s files got skipped during the last sync.",s3.skipped_files_count)
